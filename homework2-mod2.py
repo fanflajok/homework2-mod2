02edd504543b7f3b73b8be1edcf2a3655343ef2c
Victor <fanflajok@gmail.com>
2024-07-24 23:55:25 +0300

    Add files via upload
    Цель: применить навыки создания условных конструкций и знания операторов if, else, elif / and, or, not.
    
    
    Задача "Все ли равны?":
    
    На вход программе подаются 3 целых числа и записываются в переменные first, second и third соответственно.
    Ваша задача написать условную конструкцию (из if, elif, else), которая выводит кол-во одинаковых чисел среди 3-х введённых.
    
    Пункты задачи:
    
    Если все числа равны между собой, то вывести 3
    Если хотя бы 2 из 3 введённых чисел равны между собой, то вывести 2
    Если равных чисел среди 3-х вообще нет, то вывести 0

---
+++ homework2-mod2.py
@@ -0,0 +1,12 @@
+first=int(4)
+second=int(0)
+third=int(0)
+print(first)
+print(second)
+print(third)
+if first==second and second==third and first==third:
+        print(3)
+elif first == second or second==third or first == third:
+        print(2)
+else:
+        print(0)
